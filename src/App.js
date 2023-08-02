9408c57a5b1b893d1c2e0166a9b5202d06b5228b
Nihal00 <nihallsuvarna@gmail.com>
2023-08-02 09:26:39 +0530

    route in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from "./componenets/Home";
+import Blog from "./componenets/Blog";
+import Contact from "./componenets/Contact";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
+        </ul>
+
+
+        <Routes>
+          <Route exact path='/'element={<Home />} />
+          <Route exact path='/blog' element={<Blog />} />
+          <Route exact path='/contact' element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
